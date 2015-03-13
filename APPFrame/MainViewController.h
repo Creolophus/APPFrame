2a7905a6d232144112be63b90015d032bc2f6ea8
Creolophus <1715144279@qq.com>
2015-03-13 16:10:21 +0800

    提交

---
+++ APPFrame/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  APPFrame
+//
+//  Created by Creolophus on 3/13/15.
+//  Copyright (c) 2015 Creolophus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
--- APPFrame/SubViewController.h
+++ APPFrame/SubViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SubViewController.h
 //  APPFrame
 //
 //  Created by Creolophus on 3/13/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SubViewController : UIViewController
 
 @end
-
