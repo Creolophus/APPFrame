05204f98f2dcc85d5239df98e65f1bfeecb6f6f8
Creolophus <1715144279@qq.com>
2015-03-13 14:35:14 +0800

    侧滑框架

---
+++ APPFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APPFrame
+//
+//  Created by Creolophus on 3/13/15.
+//  Copyright (c) 2015 Creolophus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APPFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APPFrame
+//
+//  Created by Creolophus on 3/13/15.
+//  Copyright (c) 2015 Creolophus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
